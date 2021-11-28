24fef55dd334afa6b828776802755d8b88e4deee
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2021-11-28 10:11:48 +0330

    Add Files

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+// filter of products project : ------------------------------------
+const obj = {
+    name: "sia",
+    age: 20
+}
+console.log(JSON.stringify(obj));
+console.log(obj);
